05f61791cd00bd90b797d75799bcfad4e096f552
12200028febri <fander2483@gmail.com>
2021-12-09 09:15:21 +0700

    project final caffe and resto first commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+    public function contact()
+    {
+        return view('contact');
+    }
+}
